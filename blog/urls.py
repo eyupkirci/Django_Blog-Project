962de246585101eb7b52801fc66a6844210a46ec
eyupkirci <61272579+eyupkirci@users.noreply.github.com>
2021-10-30 01:34:28 +0300

    Urls and Views updated

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,42 @@
 from django.urls import path
-from .views import home, about, login, register
+from django.contrib.auth import views as auth_views
+
+from .views import (
+    home, 
+    about, 
+    # login, 
+    register, 
+    postadd, 
+    profile, 
+    profileupdate, 
+    profileadd,
+    like,
+    postupdate,
+    postdelete,
+    postdetail,
+    )
 
 urlpatterns = [
     path('', home, name='home'),
     path('about', about, name='about'),
-    # path('login', login, name='login'),
     path('register', register, name='register'),
+
+    # auth_views (login, logout)
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    
+    # profile
+    path('profile', profile, name='profile'),
+    path('profileadd', profileadd, name='profileadd'),
+    path('profileupdate', profileupdate, name='profileupdate'),
+
+    # post
+    path('postadd', postadd, name='postadd'),
+    path('postupdate/<int:id>', postupdate, name='postupdate'),
+    path('postdelete/<int:id>', postdelete, name='postdelete'),
+    path('postdetail/<str:slug>', postdetail, name='postdetail'),
+
+
+    # actions
+    path('like/<int:id>', like, name='like'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,203 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 
-# Create your views here.
-def home(request):
-    posts = Post.objects.all()
+from .models import Profile, Post, Like
+from .forms import PostForm, ProfileForm, RegisterForm, LikeForm
 
+from django.contrib import messages
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User 
+
+from  django.views import View
+
+
+
+# Create your views here.
+def home(request): # ok
+
+    posts = Post.objects.filter(status='active')
     context = {
         'posts': posts,
     }
     return render(request, 'home.html', context)
 
 
-def about(request):
+def about(request): # ok
     return render(request, 'about.html')
 
 
-def login(request):
-    return render(request, 'login.html')
+def register(request): # ok
+    
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Your account has been created. You can log in now!')    
+            return redirect('login')
+    else:
+        form = RegisterForm()
+
+    context = {'form': form}
+    return render(request, 'register.html', context)
 
 
-def register(request):
-    return render(request, 'register.html')
+def profile(request): # ok
+
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    posts = Post.objects.filter(user = request.user.id)
+    try:
+        profile = Profile.objects.get(user = request.user)
+    except Profile.DoesNotExist:
+        profile = None
+    
+    context = {
+        'posts': posts,
+        'profile': profile,
+    }
+
+    return render(request, 'profile.html', context)
+
+
+def profileadd(request): # ok
+
+    if not request.user.is_authenticated:
+        return redirect('login')    
+
+    form = ProfileForm()
+
+    if request.method == 'POST':
+
+        form = ProfileForm(request.POST, request.FILES)
+
+        # id otomatik olarak buradan yüklenir
+        obj = form.save(commit=False) # Return an object without saving to the DB
+        obj.user = User.objects.get(pk=request.user.id)
+
+        if form.is_valid():
+            
+            form.save()
+            messages.success(request, f'Your account has been created. You can log in now!')
+            return redirect('profile')
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'profileadd.html', context)
+
+
+def profileupdate(request): # ok
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    user = Profile.objects.get(user = request.user.id)
+    form = ProfileForm(instance=user)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Your account has been created. You can log in now!')    
+            return redirect('profile')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'profileupdate.html', context)
+
+
+def postadd(request): # ok
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    form = PostForm()
+
+    if(request.method == 'POST'):
+        form = PostForm(request.POST, request.FILES)
+
+        # id otomatik olarak buradan yüklenir
+        obj = form.save(commit=False) # Return an object without saving to the DB
+        obj.user = User.objects.get(pk=request.user.id)
+
+        if(form.is_valid()):
+            form.save()
+            return redirect('profile')
+            
+    context = {
+        'form': form,
+    }
+    return render(request, 'postadd.html', context)
+
+
+def postupdate(request, id): # ok
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    post = Post.objects.get(pk=id)
+    form = PostForm(instance=post)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'postupdate.html', context)
+
+
+def postdelete(request, id): # ok
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    post = Post.objects.get(pk=id)
+    form = PostForm(instance=post)
+
+    if request.method == 'POST':
+        post.delete()
+        return redirect('profile')
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'postdelete.html', context)
+
+
+
+# Action views
+def like(request, id):
+    
+    if request.user.is_authenticated:
+        
+        user = User.objects.get(id=request.user.id)
+        post = Post.objects.get(id=id)
+        b1 = Like(user=user, posts=post)
+
+        instance = Like.objects.filter(user=b1.user, posts=b1.posts)
+        # print(instance)
+
+        if instance:
+            instance.delete()
+        else:
+            b1.save()
+            # print("saved")
+    
+    return redirect('home')
+
+
+def postdetail(request, slug):
+
+    post = Post.objects.get(slug=slug)
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'postdetail.html', context)
