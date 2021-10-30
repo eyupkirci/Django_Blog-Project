909962136d16584303d9df430943c6c694272560
eyupkirci <61272579+eyupkirci@users.noreply.github.com>
2021-10-30 20:37:38 +0300

    settings.py is adjusted

--- main/settings.py
+++ main/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://eyups-blog-app.herokuapp.com/','localhost','127.0.0.1']
 
 
 # Application definition
